4ba92a91b013ca50a6518f97a9d2abf66bcfb65b
James Lemire <webdev.jameslemire@gmail.com>
2016-08-26 11:42:12 -0600

    Add stripe endpoint

--- CD-Store/private/server/server.js
+++ CD-Store/private/server/server.js
@@ -10,6 +10,25 @@ app.use(cors(config.corsOptions));
 app.use(bodyParser.json());
 app.use(express.static('../../public'));
 
+app.post('/api/charge', function(req,res) {
+  console.log('INCOMING PURCHASE', req.body);
+  // Get the credit card details submitted by the form
+  var token = req.body.stripeToken; // Using Express
 
+  // Create a charge: this will charge the user's card
+  var charge = stripe.charges.create({
+   amount: 1000, // Amount in cents
+   currency: "usd",
+   source: token,
+   description: "Example charge"
+  }, function(err, charge) {
+   if (err && err.type === 'StripeCardError') {
+     // The card has been declined
+   } else {
+     // Store order status
+     res.status(200).send(charge);
+   }
+ });
+});
 
 app.listen(config.port, function() { console.log("Server initiated on port", config.port); });
