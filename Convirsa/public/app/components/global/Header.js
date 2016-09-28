e89d931362927f3c0a645c997a0b7eb8c9af767a
James Lemire <webdev.jameslemire@gmail.com>
2016-09-28 11:30:06 -0600

    Finish top part of header, start bottom

--- Convirsa/public/app/components/global/Header.js
+++ Convirsa/public/app/components/global/Header.js
@@ -3,8 +3,15 @@ import React from 'react';
 export default class Header extends React.Component {
   render() {
     return (
-      <div>
-        <p>Hello from the header</p>
+      <div className="global-header">
+        <img className="global-header-logo" src="https://www.convirza.com/wp-content/themes/fluid-yeti/assets/img//header-logo.png" />
+        <div className="global-header-mobile-menu"></div>
+        <div className="global-header-contact-container">
+          <div className="global-header-contact-info-container">
+            <i class="fa fa-phone" aria-hidden="true"></i>
+            <p>CALL NOW:</p>
+          </div>
+        </div>
       </div>
     )
   }
--- Convirsa/public/app/components/home/Home.js
+++ Convirsa/public/app/components/home/Home.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
+import GlobalHeader from '../global/Header.js';
+
 require('../../../stylesheets/global/Header.scss');
 
 export default class Home extends React.Component {
   render() {
     return (
-      <div className="global-header">
-        <img className="global-header-logo" src="https://www.convirza.com/wp-content/themes/fluid-yeti/assets/img//header-logo.png" />
-        <div className="global-header-mobile-menu">
-          
-        </div>
+      <div className="home-wrapper">
+        <GlobalHeader />
       </div>
     )
   }
